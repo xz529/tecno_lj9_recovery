146d4939930dcac6b67555c31cc575aded2ec404
isus203 <128573607+isus203@users.noreply.github.com>
2026-05-27 20:45:19 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -3,6 +3,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_LJ9.mk
-
-COMMON_LUNCH_CHOICES := twrp_LJ9-eng
+     $(LOCAL_DIR)/twrp_LJ9.mk
+     
+COMMON_LUNCH_CHOICES := \
+    twrp_LJ9-user \
+    twrp_LJ9-userdebug \
+    twrp_LJ9-eng
