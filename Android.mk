7d9480e895305f526020b5eca6851369e8de676c
xz529 <1234artemich@gmail.com>
2026-05-29 15:52:49 +0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -4,5 +4,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
-$(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+ifeq ($(TARGET_DEVICE),LJ9)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
